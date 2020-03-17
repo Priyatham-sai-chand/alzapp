2b4dc9e3c2f8b07c4190ab52e020bd6eb84af0b2
rishab <rishabkulkarni1703@gmail.com>
2020-03-17 14:03:00 +0530

    Added survey

--- app/src/main/java/com/example/alzapp/UserAreaActivity.java
+++ app/src/main/java/com/example/alzapp/UserAreaActivity.java
@@ -6,14 +6,33 @@ import android.os.Bundle;
 
 import android.content.Intent;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
+
+    private Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+
+        submit= (Button) findViewById(R.id.submitUserInfo);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserAreaActivity.this,QuickPlayMenu.class);
+                startActivity(intent);
+
+            }
+        });
+
+
 
     }
 }
